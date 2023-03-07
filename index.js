334ec2d1ba44607f3d6483c1ef7574891966565d
Noé Sourdès <souredesnoe19@gmail.com>
2023-03-07 17:00:46 +0100

    finalisation de projet

---
+++ index.js
@@ -0,0 +1,57 @@
+const {randomInt} = require('crypto')
+const prompts = require('prompts')
+
+async function main(){
+    while(true){
+        const firstNumber = getFirstNumber();
+        const secondNumber = getSecondNumber();
+        const result = firstNumber*secondNumber
+        let number
+        while(number !== result){
+            number = await getNumber(firstNumber, secondNumber);
+            console.log(number);
+            if(number === result){
+                console.log('Vous avez trouvé la bonne reponse');
+                break
+            }
+            else{
+                console.log('Faux, ce n\'est pas la bonne reponse');
+                break
+            }
+        }
+        const restart = await getRestart();
+        if(restart === false){
+            break
+        }
+    }
+}
+
+function getFirstNumber(){
+    const number = randomInt(0, 10+1)
+    return number
+}
+function getSecondNumber(){
+    const number = randomInt(0, 10+1)
+    return number
+}
+
+async function getNumber(firstNumber, secondNumber){
+    const number = await prompts({
+        type: 'number',
+        name: 'value',
+        message: `Combien fait ${firstNumber}*${secondNumber} :`
+    })
+    return number.value
+}
+async function getRestart(){
+    const restart = await prompts({
+        type: 'toggle',
+        name: 'value',
+        message: 'Can you confirm?',
+        initial: true,
+        active: 'yes',
+        inactive: 'no'
+    })
+    return restart.value
+}
+main();
